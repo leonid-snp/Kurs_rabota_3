3a14cd14da3d3b0f8803db59f314d310d6662547
Леонид Петров <leonidpetrov637@gmail.com>
2024-01-30 11:11:17 +0300

    update functions.py V1.2

--- functions.py
+++ functions.py
@@ -31,6 +31,18 @@ def get_list_completed_operations(list_: list[dict, ...]) -> list[dict, ...]:
         return completed_operations
 
 
+def get_latest_transactions(list_: list[dict, ...]) -> list[dict, ...]:
+    """
+    Функция принимает список словарей и отсортировывает их по дате возвращая
+    только пять последних операций по дате
+    :param list_: список словарей
+    :return: пять первых элементов в списке
+    """
+    latest_transactions = sorted(list_, key=lambda date: date["date"], reverse=True)
+    return latest_transactions[:5]
+
+
 list_banking_transactions = get_json_file()
 list_completed_operations = get_list_completed_operations(list_banking_transactions)
-print(list_completed_operations)
+last_five_operations = get_latest_transactions(list_completed_operations)
+print(last_five_operations)
