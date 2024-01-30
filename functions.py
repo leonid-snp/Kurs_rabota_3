6326d42e5d4ec781cd0590d2fd4cb812524bff34
Леонид Петров <leonidpetrov637@gmail.com>
2024-01-30 10:40:40 +0300

    update functions.py V1.1

--- functions.py
+++ functions.py
@@ -11,4 +11,26 @@ def get_json_file() -> list[dict, ...]:
         return file
 
 
-list_of_banking_transactions = get_json_file()
+def get_list_completed_operations(list_: list[dict, ...]) -> list[dict, ...]:
+    """
+    Функция принимает список словарей и сортирует его по выполненным операциям
+    :param list_: список словарей
+    :return: список словарей
+    """
+    try:
+        completed_operations = []
+        for state in list_:
+            if state["state"] == "EXECUTED":
+                completed_operations.append(state)
+        return completed_operations
+
+    except KeyError:
+        pass
+
+    finally:
+        return completed_operations
+
+
+list_banking_transactions = get_json_file()
+list_completed_operations = get_list_completed_operations(list_banking_transactions)
+print(list_completed_operations)
